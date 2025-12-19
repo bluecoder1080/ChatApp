337c0f11d2c7ecab2533bf176fd6a4e90c9b656c
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2025-12-20 00:51:43 +0530

    feat: store connected sockets in an array

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets = [];
 
 wss.on("connection", (socket) => {
+  allSockets.push(socket);
   console.log("User Connected");
   userCount++;
   console.log("User Connected #" + userCount);
